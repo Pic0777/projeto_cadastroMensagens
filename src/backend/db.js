648b385db3bd5b278dd1fb9d2419037dee5d887b
Pic0777 <162361580+Pic0777@users.noreply.github.com>
2025-09-02 17:33:56 -0300

    mudando conexão

--- src/backend/db.js
+++ src/backend/db.js
@@ -8,12 +8,7 @@ console.log("🟢 Variáveis carregadas:", {
   database: process.env.DB_NAME
 });
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+const connection = mysql.createConnection(process.env.MYSQL_URL);
 
 connection.connect((err)=>{
     if (err){
